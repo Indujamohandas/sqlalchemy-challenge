2d63eb3eec331fb440ef8f37f6b0d971f8ef1812
Induja <indujamohandas@gmail.com>
2023-08-25 10:13:02 -0500

    file added

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,18 +1,28 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///SurfsUp/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
+Base.prepare(autoload_with=engine)
 
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
-# Save references to each table
 
 
 # Create our session (link) from Python to the DB
@@ -22,9 +32,11 @@
 # Flask Setup
 #################################################
 
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/api/v1.0/precipitation")
+
